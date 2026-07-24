4f0d212cb91d534af3010a914e323cf32adfe3ec
freetrick009 <moyurbora7@gmail.com>
2026-07-24 09:38:00 +0530

    Update welcome message and improve formatting

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
   ctx.reply(
-    "🎉 Welcome!\n\nTap the button below to open the Mini App.",
-    Markup.keyboard([
-      [
-        Markup.button.webApp(
-          "🎁 Open Mini App",
-          "https://nestle-kitkat-reward.netlify.app"
-        )
-      ]
-    ]).resize()
+    "🍫 *Welcome to the Nestlé KitKat Cashback Rewards!* 🎉\n\n🎁 Claim your exclusive cashback in just a few simple steps.\n\n👇 Tap the button below to get started!",
+    {
+      parse_mode: "Markdown",
+      ...Markup.keyboard([
+        [
+          Markup.button.webApp(
+            "🎁 Open Mini App",
+            "https://nestle-kitkat-reward.netlify.app"
+          )
+        ]
+      ]).resize()
+    }
   );
 });
 
