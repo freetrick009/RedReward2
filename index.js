c980f920b3b1ac1f41487cd4169f5a022f3cec81
freetrick009 <moyurbora7@gmail.com>
2026-07-24 09:46:11 +0530

    Enhance bot start command with image and button
    Updated the bot's start command to send a welcome image and a Mini App button.

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const { Telegraf, Markup } = require("telegraf");
+const path = require("path");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => {
-  ctx.reply(
-    "🍫 *Welcome to the Nestlé KitKat Cashback Rewards!* 🎉\n\n🎁 Claim your exclusive cashback in just a few simple steps.\n\n👇 Tap the button below to get started!",
+bot.start(async (ctx) => {
+  const imagePath = path.join(__dirname, "assets", "welcome.jpg");
+
+  // Send welcome image
+  await ctx.replyWithPhoto(
+    { source: imagePath },
     {
+      caption:
+        "🍫 *Welcome to the Nestlé KitKat Cashback Rewards!* 🎉\n\n🎁 Claim your exclusive cashback in just a few simple steps.\n\n👇 Tap the button below to get started!",
       parse_mode: "Markdown",
-      ...Markup.keyboard([
-        [
-          Markup.button.webApp(
-            "🎁 Open Mini App",
-            "https://nestle-kitkat-reward.netlify.app"
-          )
-        ]
-      ]).resize()
     }
+  );
+
+  // Send Mini App button
+  await ctx.reply(
+    "👇 Open the Mini App",
+    Markup.keyboard([
+      [
+        Markup.button.webApp(
+          "🎁 Open Mini App",
+          "https://nestle-kitkat-reward.netlify.app"
+        ),
+      ],
+    ])
+      .resize()
+      .oneTime()
   );
 });
 
