784ef733433341a48d6d8a0e12a91b14ae4a74de
freetrick009 <moyurbora7@gmail.com>
2026-07-24 09:52:40 +0530

    Refactor welcome message and button handling

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ const path = require("path");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(async (ctx) => {
-  const imagePath = path.join(__dirname, "assets", "welcome.jpg");
+  try {
+    const imagePath = path.join(__dirname, "assets", "welcome.jpg");
 
-  // Send welcome image
-  await ctx.replyWithPhoto(
-    { source: imagePath },
-    {
-      caption:
-        "🍫 *Welcome to the Nestlé KitKat Cashback Rewards!* 🎉\n\n🎁 Claim your exclusive cashback in just a few simple steps.\n\n👇 Tap the Claim button below to get started!",
-      parse_mode: "Markdown",
-    }
-  );
+    // Send welcome image
+    await ctx.replyWithPhoto(
+      { source: imagePath },
+      {
+        caption:
+          "🍫 *Welcome to the Nestlé KitKat Cashback Rewards!* 🎉\n\n🎁 Claim your exclusive cashback in just a few simple steps.\n\n👇 Tap the button below to get started!",
+        parse_mode: "Markdown",
+      }
+    );
 
-  // Send Mini App button
-  await ctx.reply(
-    "
-    Markup.keyboard([
-      [
-        Markup.button.webApp(
-          "🎁 Open Mini App",
-          "https://nestle-kitkat-reward.netlify.app"
-        ),
-      ],
-    ])
-      .resize()
-      .oneTime()
-  );
+    // Send Web App button
+    await ctx.reply(
+      " ",
+      Markup.keyboard([
+        [
+          Markup.button.webApp(
+            "🎁 Open Mini App",
+            "https://nestle-kitkat-reward.netlify.app"
+          ),
+        ],
+      ]).resize()
+    );
+  } catch (err) {
+    console.error(err);
+    ctx.reply("❌ Something went wrong.");
+  }
 });
 
 bot.launch();
